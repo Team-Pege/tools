f8b8692b5ce75ff98ae7c3eb8e471ba29a4c4c8c
pegedev <pege.dev7878@gmail.com>
2025-01-25 12:35:43 +0700

    add options validation

--- L7/CARS.js
+++ L7/CARS.js
@@ -202,7 +202,7 @@ class NetSocket {
         const addrHost = parsedAddr[0];
         const payload = "CONNECT " + options.address + ":443 HTTP/1.1\r\nHost: " + options.address + ":443\r\nConnection: Keep-Alive\r\n\r\n";
         const buffer = new Buffer.from(payload);
-        console.log({options})
+        if (!options.host || !options.port) return;
         const connection = net.connect({
             host: options.host,
             port: options.port,
